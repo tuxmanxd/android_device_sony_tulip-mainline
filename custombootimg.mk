762b0d5f16699f366ce0b10b2fb0f92ca5d7e617
TuxMan <79749594+tuxmanxd@users.noreply.github.com>
2026-04-27 20:22:33 +0100

    sony-tulip: Misc changes

--- custombootimg.mk
+++ custombootimg.mk
@@ -3,13 +3,13 @@ KERNEL_OUT := $(PRODUCT_OUT)/obj/KERNEL_OBJ
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TULIP_DTB := $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/dts/qcom/msm8939-sony-xperia-kanuti-tulip.dtb
 
-define make-tux-kernel
+define make-kernel
 	$(hide) cat $(PRODUCT_OUT)/kernel $(TULIP_DTB) > $(PRODUCT_OUT)/kernel-dtb
 endef
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(TARGET_PREBUILT_INT_KERNEL)
-	$(call pretty,"Bootimage built successfully!: $@")
-	$(make-tux-kernel)
+	$(call pretty,"Success!: $@")
+	$(make-kernel)
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel-dtb \
 		--ramdisk $(PRODUCT_OUT)/ramdisk.img \
 		--base 0x80000000 --pagesize 2048 \
@@ -18,7 +18,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(TARGET
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) $(TARGET_PREBUILT_INT_KERNEL)
 	$(call pretty,"DONE!: $@")
-	$(make-tux-kernel)
+	$(make-kernel)
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel-dtb \
 		--ramdisk $(recovery_ramdisk) \
 		--base 0x80000000 --pagesize 2048 \
